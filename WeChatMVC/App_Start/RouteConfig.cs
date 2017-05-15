9f2a34a663b43e8731b8cd9ea4cccb4f2b76d379
Nanxing Xiao <506830915@qq.com>
2017-05-15 23:24:28 +0800

    finish html

--- WeChatMVC/App_Start/RouteConfig.cs
+++ WeChatMVC/App_Start/RouteConfig.cs
@@ -14,23 +14,30 @@ namespace WeChatMVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "WeChat",
+                url: "wechat/{action}/{id}",
+                defaults: new { controller = "wechat", action = "index", id = UrlParameter.Optional }
             );
 
             routes.MapRoute(
                 name: "useAPI",
                 url: "api/{action}/{studentnum}/{pwd}",
-                defaults: new { controller = "api", action = "action", id = UrlParameter.Optional },
+                defaults: new { controller = "api", id = UrlParameter.Optional },
                 constraints: new { studentnum = @"\d*" }
                 );
 
             routes.MapRoute(
                 name: "FileUpLoad",
-                url: "fileuploada/{action}",
+                url: "fileupload/{action}",
                 defaults: new { controller = "fileupload", action = "index", id = UrlParameter.Optional }
                 );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "wechat", action = "index", id = UrlParameter.Optional }
+            );
+
         }
     }
 }
---
+++ WeChatMVC/Controllers/WeChatController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+using System.Xml;
+using System.Data;
+using System.Web.Security;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
+
+namespace WeChatMVC.Controllers
+{
+    public class WeChatController : Controller
+    {
+        // GET: WeChat
+        public string Index() //回复全都是xml格式的string
+        {
+            if(checkSignature())
+            {
+                if (Request.HttpMethod == "POST")
+                {
+                    UserRequest test = new UserRequest(Request.InputStream);
+                    return test.Get_Reply("test");
+                }
+            }
+            return "Don't touch this server,guy";
+        }
+
+        public bool checkSignature()
+        {
+            var signature = Request["signature"];
+            var timestamp = Request["timestamp"];
+            var nonce = Request["nonce"];
+            var token = "961016";
+            string[] ArrTmp = { token, timestamp, nonce };
+            Array.Sort(ArrTmp);     //字典排序  
+            string tmpStr = string.Join("", ArrTmp);
+            tmpStr = FormsAuthentication.HashPasswordForStoringInConfigFile(tmpStr, "SHA1");
+            tmpStr = tmpStr.ToLower();
+            if (tmpStr == signature)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }//信息来源是腾讯才会返回true
+    }
+
+    public class UserRequest
+    {
+        /*
+         * 所有Get.*_Relpy的函数，都返回直接能回复的xml格式字符
+         */
+        public string ToUserName;
+        public string FromUserName;
+        public string CreateTime;
+        public string MsgType;
+        public string Content;
+        public string MsgId;
+        public string PicUrl;
+        public string MediaId;
+        public UserRequest(XmlDocument doc)
+        {
+            XmlElement xe = doc.DocumentElement;
+            fillclass(xe);
+        }
+        public UserRequest(string xml)
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.LoadXml(xml);
+            XmlElement xe = xmldoc.DocumentElement;
+            fillclass(xe);
+        }
+        public UserRequest(Stream xmlstream)
+        {
+            StreamReader sr = new StreamReader(xmlstream);
+            string xml = sr.ReadToEnd();
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.LoadXml(xml);
+            XmlElement xe = xmldoc.DocumentElement;
+            fillclass(xe);
+        }
+        private void fillclass(XmlElement xe)
+        {
+            ToUserName = xe.SelectSingleNode("ToUserName").InnerText;
+            FromUserName = xe.SelectSingleNode("FromUserName").InnerText;
+            CreateTime = xe.SelectSingleNode("CreateTime").InnerText;
+            MsgType = xe.SelectSingleNode("MsgType").InnerText;
+            if (MsgType == "text")
+            {
+                Content = xe.SelectSingleNode("Content").InnerText;
+            }
+            else if (MsgType == "image")
+            {
+                MediaId = xe.SelectSingleNode("MediaId").InnerText;
+                PicUrl = xe.SelectSingleNode("PicUrl").InnerText;
+            }
+            MsgId = xe.SelectSingleNode("MsgId").InnerText;
+        }
+        public string Get_MJ_Reply(DateTime buyday, DateTime ticketday, string origination, string destination)
+        {
+            string url_origination = get_urlEncode(origination);
+            string url_destination = get_urlEncode(destination);
+            string url = "http://ecust.top/mj/html/index.php?buy_year=" +
+                buyday.Year + "&buy_month=" +
+                buyday.Month + "&buy_day=" +
+                buyday.Day + "&buy_hour=" +
+                buyday.Hour + "&buy_minute=" +
+                buyday.Minute + "&stationary=0&origination=" +
+                url_origination + "&destination=" +
+                url_destination + "&ticket_year=" +
+                ticketday.Year + "&ticket_month=" +
+                ticketday.Month + "&ticket_day=" +
+                ticketday.Day + "&ticket_hour=" +
+                ticketday.Hour + "&ticket_minute=" +
+                ticketday.Minute + "&ticket_year=" +
+                ticketday.Year + "&ticket_month=" +
+                ticketday.Month + "&ticket_day=" +
+                ticketday.Day + "&ticket_hour=" +
+                ticketday.Hour + "&ticket_minute=" +
+                ticketday.Minute;
+
+            return Get_Reply(Get_Link_Reply(url, "车票"));
+        }
+        private string get_urlEncode(string input)
+        {
+            return HttpUtility.UrlEncode(input);
+        }
+        public string Get_Link_Reply(string url, string text) 
+        {
+            return Get_Reply("<a href=\"" + url + "\">" + text + "</a>");
+        }
+        public string Get_Reply(string content)
+        {
+            string reply = "<xml><ToUserName><![CDATA[" + FromUserName +
+                "]]></ToUserName><FromUserName><![CDATA[" + ToUserName +
+            "]]></FromUserName><CreateTime>" + ConvertDateTimeInt(DateTime.Now) +
+            "</CreateTime><MsgType><![CDATA[" + "text" +
+            "]]></MsgType><Content><![CDATA[" + content +
+            "]]></Content></xml>";
+            return reply;
+        }//返回最终的xml
+        public static int ConvertDateTimeInt(DateTime time)
+        {
+            DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            return (int)(time - startTime).TotalSeconds;
+        }
+    }
+}
