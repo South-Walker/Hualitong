aad37e309cfa712165c0dae1f9847053a3348cf6
Nanxing Xiao <506830915@qq.com>
2017-06-20 20:26:52 +0800

    2017-6-20

---
+++ WeChatMVC/Controllers/BaiduMapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeChatMVC.Controllers
+{
+    public class BaiduMapController : Controller
+    {
+        // GET: BaiduMap
+        public ActionResult Index()
+        {
+            return View("MapView");
+        }
+    }
+}
